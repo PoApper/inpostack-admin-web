ff0864a94a5998af0bf397961b8429094b4507bc
Seokyun Ha <bluehornblues@gmail.com>
2021-12-29 09:29:54 +0900

    separate overview box

--- components/index/overview-block.jsx
+++ components/index/overview-block.jsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react'
-import Link from 'next/link'
 import { Mobile, PC } from '../MediaQuery'
-import styled from 'styled-components'
-import { Grid, Icon } from 'semantic-ui-react'
+import { Grid } from 'semantic-ui-react'
 import axios from 'axios'
+
+import OverviewBox from "./overview-box";
 
 const OverviewBlock = () => {
   const [overview, setOverview] = useState({})
@@ -15,62 +15,38 @@ const OverviewBlock = () => {
      .catch((err) => alert('Overview를 불러오는데 실패했습니다.'))
   }, [])
 
-  const getOverviewElement = ({ header, number, link }) => {
-    return (
-      <MainBox>
-        <h3>{header}</h3>
-        <p>{number}</p>
-        <div
-          style={{ gridArea: 'blank', width: '80px', margin: 0, padding: 0 }}/>
-        <Link href={link}>
-          <div style={{
-            gridArea: 'whole',
-            margin: '-24px -25px -24px',
-            padding: '55px 0 0',
-            backgroundColor: '#265c71',
-            color: '#fff',
-            textAlign: 'center',
-            cursor: 'pointer',
-          }}>
-            <Icon name="arrow right"/>
-          </div>
-        </Link>
-      </MainBox>
-    )
-  }
-
   return (
     <div>
       <PC>
         <Grid columns={4} style={{ marginTop: '30px' }}>
           <Grid.Row stretched>
             <Grid.Column>
-              {getOverviewElement({
-                header: '전체 사용자 수',
-                number: overview['account_count'],
-                link: '/account',
-              })}
+              <OverviewBox
+                header={'전체 사용자 수'}
+                number={overview['account_count']}
+                link={'/account'}
+              />
             </Grid.Column>
             <Grid.Column>
-              {getOverviewElement({
-                header: '전체 가게 수',
-                number: overview['store_count'],
-                link: '/store',
-              })}
+              <OverviewBox
+                header={'전체 가게 수'}
+                number={overview['store_count']}
+                link={'/store'}
+              />
             </Grid.Column>
             <Grid.Column>
-              {getOverviewElement({
-                header: '전체 공지 수',
-                number: overview['notice_count'],
-                link: '/notice',
-              })}
+              <OverviewBox
+                header={'전체 공지 수'}
+                number={overview['notice_count']}
+                link={'/notice'}
+              />
             </Grid.Column>
             <Grid.Column>
-              {getOverviewElement({
-                header: '전체 리뷰 수',
-                number: overview['review_count'],
-                link: '/review',
-              })}
+              <OverviewBox
+                header={'전체 리뷰 수'}
+                number={overview['review_count']}
+                link={'/review'}
+              />
             </Grid.Column>
           </Grid.Row>
         </Grid>
@@ -79,38 +55,38 @@ const OverviewBlock = () => {
         <Grid columns={1} style={{ marginTop: '30px' }}>
           <Grid.Row stretched>
             <Grid.Column>
-              {getOverviewElement({
-                header: '전체 사용자 수',
-                number: overview['account_count'],
-                link: '/account',
-              })}
+              <OverviewBox
+                header={'전체 사용자 수'}
+                number={overview['account_count']}
+                link={'/account'}
+              />
             </Grid.Column>
           </Grid.Row>
           <Grid.Row stretched>
             <Grid.Column>
-              {getOverviewElement({
-                header: '전체 가게 수',
-                number: overview['store_count'],
-                link: '/store',
-              })}
+              <OverviewBox
+                header={'전체 가게 수'}
+                number={overview['store_count']}
+                link={'/store'}
+              />
             </Grid.Column>
           </Grid.Row>
           <Grid.Row stretched>
             <Grid.Column>
-              {getOverviewElement({
-                header: '전체 공지 수',
-                number: overview['notice_count'],
-                link: '/notice',
-              })}
+              <OverviewBox
+                header={'전체 공지 수'}
+                number={overview['notice_count']}
+                link={'/notice'}
+              />
             </Grid.Column>
           </Grid.Row>
           <Grid.Row stretched>
             <Grid.Column>
-              {getOverviewElement({
-                header: '전체 리뷰 수',
-                number: overview['review_count'],
-                link: '/review',
-              })}
+              <OverviewBox
+                header={'전체 리뷰 수'}
+                number={overview['review_count']}
+                link={'/review'}
+              />
             </Grid.Column>
           </Grid.Row>
         </Grid>
@@ -120,40 +96,3 @@ const OverviewBlock = () => {
 }
 
 export default OverviewBlock
-
-const MainBox = styled.div`
-  display: grid;
-  grid-template-areas:
-		"header blank whole"
-		"number blank whole";
-  overflow: hidden;
-
-  border-radius: 14px;
-  background-color: #fff;
-  box-shadow: 4px 12px 30px 6px rgb(0 0 0 / 9%);
-  padding: 25px 24px 25px;
-  transition: all 200ms;
-
-  &:hover {
-    transform: translateY(-5px);
-    box-shadow: 3px 11px 28px 4px rgb(0 0 0 / 20%);
-  }
-
-  h6 {
-    grid-area: header;
-    color: #888;
-    font-size: 12px;
-    margin: 5px 0 0;
-  }
-
-  p {
-    grid-area: number;
-    font-size: 35px;
-    margin: auto;
-    text-align: center;
-  }
-
-  h3 {
-    text-align: right;
-  }
-`
---
+++ components/index/overview-box.jsx
@@ -0,0 +1,66 @@
+import Link from "next/link";
+import styled from "styled-components";
+import {Icon} from "semantic-ui-react";
+
+const OverviewBox = ({header, number, link}) => {
+  return (
+    <MainBox>
+      <h3>{header}</h3>
+      <p>{number}</p>
+      <div
+        style={{ gridArea: 'blank', width: '80px', margin: 0, padding: 0 }}/>
+      <Link href={link}>
+        <div style={{
+          gridArea: 'whole',
+          margin: '-24px -25px -24px',
+          padding: '55px 0 0',
+          backgroundColor: '#265c71',
+          color: '#fff',
+          textAlign: 'center',
+          cursor: 'pointer',
+        }}>
+          <Icon name="arrow right"/>
+        </div>
+      </Link>
+    </MainBox>
+  )
+}
+
+export default OverviewBox
+
+const MainBox = styled.div`
+  display: grid;
+  grid-template-areas:
+		"header blank whole"
+		"number blank whole";
+  overflow: hidden;
+
+  border-radius: 14px;
+  background-color: #fff;
+  box-shadow: 4px 12px 30px 6px rgb(0 0 0 / 9%);
+  padding: 25px 24px 25px;
+  transition: all 200ms;
+
+  &:hover {
+    transform: translateY(-5px);
+    box-shadow: 3px 11px 28px 4px rgb(0 0 0 / 20%);
+  }
+
+  h6 {
+    grid-area: header;
+    color: #888;
+    font-size: 12px;
+    margin: 5px 0 0;
+  }
+
+  p {
+    grid-area: number;
+    font-size: 35px;
+    margin: auto;
+    text-align: center;
+  }
+
+  h3 {
+    text-align: right;
+  }
+`
