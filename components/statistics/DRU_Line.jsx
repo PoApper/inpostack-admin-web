b3a395df4eb0c2be56832e6439599c72dfb7d9d3
bluehorn07 <bluehornblues@gmail.com>
2022-01-06 22:02:50 +0900

    fix DRU API call

--- components/statistics/DRU_Line.jsx
+++ components/statistics/DRU_Line.jsx
@@ -6,15 +6,12 @@ import moment from 'moment'
 const DRU_Line = () => {
   const [DRU_data, setDRU_data] = useState([])
 
-  useEffect(async () => {
-    try {
-      const today = moment().format('YYYY-MM-DD')
-      const res = await axios.get(
-        `${process.env.NEXT_PUBLIC_API}/statistics/DRU?start_date=${'2021-08-01'}&end_date=${today}`);
-      setDRU_data(res.data);
-    } catch (err) {
-      alert("통계 데이터를 불러오는데 실패했습니다.")
-    }
+  useEffect(() => {
+    const today = moment().format('YYYY-MM-DD')
+    axios
+      .get(`${process.env.NEXT_PUBLIC_API}/statistics/daily_register_user?start_date=${'2021-08-01'}&end_date=${today}`)
+      .then((res) => setDRU_data(res.data))
+      .catch(() => alert('Daily Register User를 불러오는데 실패했습니다.'))
   }, [])
 
   const data = {
@@ -22,7 +19,7 @@ const DRU_Line = () => {
     datasets: [
       {
         label: '# of register user',
-        data: DRU_data.map(row => row.DRU),
+        data: DRU_data.map(row => row.daily_register_user),
         fill: false,
         backgroundColor: 'rgb(255, 99, 132)',
         borderColor: 'rgba(255, 99, 132, 0.2)',
