09b29eb5b50fe31a97e29ed171e369bc9867c901
bluehorn07 <bluehornblues@gmail.com>
2022-01-10 22:30:08 +0900

    separate menuModal to create/update

--- components/menu/menuCreateModal.jsx
+++ components/menu/menuCreateModal.jsx
@@ -2,10 +2,9 @@ import React, { useState } from 'react'
 import { Form, Icon, Message, Modal } from 'semantic-ui-react'
 import { useRouter } from 'next/router'
 import axios from 'axios'
-import theme from '../../styles/theme'
 import styled from 'styled-components'
 
-export const MenuCreateModal = (props) => {
+const MenuCreateModal = (props) => {
   const router = useRouter()
   const [isModalOpen, setModalOpen] = useState(false)
 
@@ -122,143 +121,7 @@ export const MenuCreateModal = (props) => {
   )
 }
 
-export const MenuUpdateModal = (props) => {
-  const router = useRouter()
-  const [isModalOpen, setModalOpen] = useState(false)
-
-  const menuInfo = props.menuInfo
-
-  const [name, setName] = useState(menuInfo.name)
-  const [description, setDescription] = useState(menuInfo.description)
-  const [price, setPrice] = useState(menuInfo.price)
-  const [image_url, setImageUrl] = useState(menuInfo.image_url)
-  const [newMenuImg, setNewMenuImg] = useState()
-
-  async function handleUpdate (e) {
-    e.preventDefault()
-
-    const formData = new FormData()
-    formData.append('category_uuid', props.categoryUUID)
-    formData.append('name', name)
-    formData.append('description', description)
-    formData.append('price', price)
-    if (newMenuImg) {
-      formData.append('menu_img', newMenuImg)
-    }
-
-    try {
-      await axios.put(`${process.env.NEXT_PUBLIC_API}/menu/${menuInfo.uuid}`, formData,
-        { withCredentials: true })
-      alert('메뉴를 수정 했습니다.')
-      router.reload(window.location.pathname)
-    } catch (err) {
-      alert('메뉴 수정에 실패했습니다.')
-    }
-  }
-
-  async function handleDelete (e) {
-    e.preventDefault()
-
-    try {
-      await axios.delete(
-        `${process.env.NEXT_PUBLIC_API}/menu/${menuInfo.uuid}`,
-        { withCredentials: true })
-      alert('메뉴를 삭제 했습니다.')
-      router.reload(window.location.pathname)
-    } catch (err) {
-      alert('메뉴 삭제에 실패했습니다.')
-    }
-  }
-
-  return (
-    <Modal
-      size="small"
-      open={isModalOpen}
-      trigger={props.trigger}
-      onClose={() => setModalOpen(false)}
-      onOpen={() => setModalOpen(true)}
-    >
-      <Modal.Header>메뉴 수정: {menuInfo.name}</Modal.Header>
-      <Modal.Content>
-        <Form onSubmit={handleUpdate}>
-          <div style={{display: 'flex', justifyContent:'space-between', marginBottom: '20px'}}>
-          <Left>
-          <Form.Input
-            required
-            label={'메뉴 이름'}
-            value={name}
-            placeholder={'생성할 메뉴 이름을 입력해주세요.'}
-            onChange={(e) => setName(e.target.value)}
-          />
-
-          <Form.Input
-            required
-            label={'가격'}
-            value={price}
-            placeholder={'가격을 입력해주세요.'}
-            onChange={(e) => setPrice(e.target.value)}
-          />
-
-          <Form.TextArea
-            required
-            label={'메뉴 설명'}
-            value={description}
-            placeholder={'ex. 2인분 이상 배달 가능\n원산지: 호주\n소: 5,000, 중: 8,000, 대: 12,000'}
-            onChange={(e) => setDescription(e.target.value)}
-          />
-
-          <Form.Input disabled
-            label={'카테고리'}
-            value={props.categoryName}
-          />
-          </Left>
-          <Right>
-            <Form.Field>
-              <label>메뉴 사진</label>
-              <ImgBox>
-                <img width={150} height={150}
-                    src={image_url ??
-                    'https://via.placeholder.com/150?text=InPostack'}
-                    alt="menu_photo"/>
-              </ImgBox>
-              <FileBox>
-                <label>
-                  <span>업로드</span>
-                  <input
-                    type="file" accept="image/*" name="menu_image"
-                    onChange={(evt) => {
-                      const file = evt.target.files[0]
-                      const fileReader = new FileReader()
-                      fileReader.onloadend = () => {
-                        setImageUrl(fileReader.result)
-                        setNewMenuImg(file)
-                      }
-                      fileReader.readAsDataURL(file)
-                    }}/>
-                </label>
-              </FileBox>
-            </Form.Field>
-          </Right>
-          </div>
-          <Form.Group>
-            <Form.Button>
-              <Icon name="save" style={{ marginRight: '0.5rem' }}/>
-              저장
-            </Form.Button>
-            <Form.Button
-              style={{
-                backgroundColor: theme.color.inpostack_red2, color: 'white',
-              }}
-              onClick={handleDelete}>
-              <Icon name="delete" style={{ marginRight: '0.5rem' }}/>
-              삭제
-            </Form.Button>
-          </Form.Group>
-        </Form>
-      </Modal.Content>
-    </Modal>
-  )
-}
+export default MenuCreateModal
 
 const ImgBox = styled.div`
   margin-top: 15px;
