cb97ca3157dcecb64f10d6331026d6634847cc16
hodori314 <hodori@postech.ac.kr>
2022-01-27 21:24:21 +0900

    change chart lib to nivo for DRU

--- components/statistics/DRU_Line.jsx
+++ components/statistics/DRU_Line.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
-import { Line } from 'react-chartjs-2'
+import { ResponsiveLine } from '@nivo/line'
 import moment from 'moment'
 
 const DRU_Line = () => {
@@ -20,9 +20,6 @@ const DRU_Line = () => {
       {
         label: '# of register user',
         data: DRUValue.map(row => row.daily_register_user),
-        fill: false,
-        backgroundColor: 'rgb(255, 99, 132)',
-        borderColor: 'rgba(255, 99, 132, 0.2)',
       },
     ],
   }
@@ -30,7 +27,75 @@ const DRU_Line = () => {
   return (
     <div>
       <h2>일일 가입 유저</h2>
-      <Line data={data}/>
+      <div style={{height: '500px'}}>
+        <ResponsiveLine
+          data={data}
+          margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
+          xScale={{ type: 'point' }}
+          yScale={{ type: 'linear', min: 'auto', max: 'auto', stacked: true, reverse: false }}
+          yFormat=" >-.2f"
+          curve="catmullRom"
+          axisTop={null}
+          axisRight={null}
+          axisBottom={{
+              orient: 'bottom',
+              tickSize: 5,
+              tickPadding: 5,
+              tickRotation: 0,
+              legend: 'transportation',
+              legendOffset: 36,
+              legendPosition: 'middle'
+          }}
+          axisLeft={{
+              orient: 'left',
+              tickSize: 5,
+              tickPadding: 5,
+              tickRotation: 0,
+              legend: 'count',
+              legendOffset: -40,
+              legendPosition: 'middle'
+          }}
+          enableGridX={false}
+          enableGridY={false}
+          colors={{ scheme: 'set1' }}
+          lineWidth={4}
+          pointColor={{ theme: 'background' }}
+          pointBorderWidth={2}
+          pointBorderColor={{ from: 'serieColor', modifiers: [] }}
+          pointLabel="x"
+          pointLabelYOffset={-12}
+          areaOpacity={0.1}
+          enableCrosshair={false}
+          useMesh={true}
+          legends={[
+              {
+                  anchor: 'bottom-right',
+                  direction: 'column',
+                  justify: false,
+                  translateX: 100,
+                  translateY: 0,
+                  itemsSpacing: 0,
+                  itemDirection: 'left-to-right',
+                  itemWidth: 80,
+                  itemHeight: 23,
+                  itemOpacity: 0.75,
+                  symbolSize: 13,
+                  symbolShape: 'circle',
+                  symbolBorderColor: 'rgba(0, 0, 0, .5)',
+                  effects: [
+                      {
+                          on: 'hover',
+                          style: {
+                              itemBackground: 'rgba(0, 0, 0, .03)',
+                              itemOpacity: 1
+                          }
+                      }
+                  ]
+              }
+          ]}
+          motionConfig="stiff"
+      />
+    </div>
     </div>
   )
 
