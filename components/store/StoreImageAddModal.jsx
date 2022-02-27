9e76921689f1c1ab475886c720dad44394e291a9
bluehorn07 <bluehornblues@gmail.com>
2022-02-28 01:21:05 +0900

    refine store image upload modal

--- components/store/StoreImageAddModal.jsx
+++ components/store/StoreImageAddModal.jsx
@@ -1,17 +1,22 @@
-import { Button, Icon, Modal } from 'semantic-ui-react'
+import {
+  Button,
+  Divider,
+  Header,
+  Icon,
+  Modal,
+  Segment,
+} from 'semantic-ui-react'
 import React, { useState } from 'react'
 import axios from 'axios'
 import styled from 'styled-components'
 import { useRouter } from 'next/router'
+import Dropzone from 'react-dropzone'
 
 const StoreImageAddModal = ({ storeInfo }) => {
-  const router = useRouter();
+  const router = useRouter()
 
   const [open, setOpen] = useState(false)
-  const [imageUrl, setImageUrl] = useState('')
   const [newStoreImg, setNewStoreImg] = useState()
-
-  console.log(storeInfo)
 
   function uploadStoreImage () {
     const formData = new FormData()
@@ -19,8 +24,8 @@ const StoreImageAddModal = ({ storeInfo }) => {
     axios.post(`${process.env.NEXT_PUBLIC_API}/store-image/${storeInfo.uuid}`,
       formData, { withCredentials: true },
     ).then(() => {
-      alert('가게 이미지가 등록되었습니다!');
-      router.reload();
+      alert('가게 이미지가 등록되었습니다!')
+      router.reload()
     }).catch((err) => {
       alert('가게 이미지 등록에 실패했습니다.')
       console.log(err)
@@ -36,22 +41,38 @@ const StoreImageAddModal = ({ storeInfo }) => {
     >
       <Modal.Header>가게 사진 추가</Modal.Header>
       <Modal.Content>
-        <FileBox>
-          <label>
-            <span>업로드</span>
-            <input
-              type={'file'} accept={'image/*'}
-              onChange={(evt) => {
-                const file = evt.target.files[0]
-                const fileReader = new FileReader()
-                fileReader.onloadend = () => {
-                  setImageUrl(fileReader.result)
-                  setNewStoreImg(file)
-                }
-                fileReader.readAsDataURL(file)
-              }}/>
-          </label>
-        </FileBox>
+        {
+          newStoreImg ? (
+            <div>
+              {/* TODO: add preview image here! */}
+              <Header>
+                이미지를 업로드하시겠습니까?
+              </Header>
+            </div>
+          ) : (
+            <Dropzone onDrop={acceptedFiles => setNewStoreImg(acceptedFiles[0])}>
+              {
+                ({ getRootProps, getInputProps }) => (
+                  <section>
+                    <Segment placeholder {...getRootProps()}>
+                      <Header icon>
+                        <Icon name="image file outline" style={{ height: 60 }}/>
+                        <p>
+                          가게에 추가할 이미지를 이곳에 놓으세요!
+                        </p>
+                      </Header>
+                      <Button primary>Add Image</Button>
+                      <input {...getInputProps()} type={'file'}
+                             accept={'image/*'}/>
+                    </Segment>
+                  </section>
+                )}
+            </Dropzone>
+          )
+        }
+
+        <Divider/>
+
         <div>
           <Button onClick={uploadStoreImage}>
             업로드
