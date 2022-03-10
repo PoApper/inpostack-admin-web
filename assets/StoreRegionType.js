18c08e50ca401d3ea6476dc4daf88223cc9a5bff
bluehorn07 <bluehornblues@gmail.com>
2022-03-10 21:28:15 +0900

    replace store meta to const asset

---
+++ assets/StoreRegionType.js
@@ -0,0 +1,30 @@
+// should be sync with enum defined in API
+const StoreRegionType = [
+  {
+    key: 'hyoja',
+    value: 'hyo-ja',
+    text: '효자 시장',
+  },
+  {
+    key: 'skview',
+    value: 'sk-view',
+    text: 'SK뷰',
+  },
+  {
+    key: 'yugang',
+    value: 'yu-gang',
+    text: '유강',
+  },
+  {
+    key: 'yidong',
+    value: 'yi-dong',
+    text: '이동',
+  },
+  {
+    key: 'jigok',
+    value: 'ji-gok',
+    text: '지곡',
+  },
+];
+
+export default StoreRegionType;
