1fd12881d2fb2fa4a3a8ec580351cbd943bfa58f
bluehorn07 <bluehornblues@gmail.com>
2022-03-23 00:06:29 +0900

    add store-image delete logic and separate store-image component

--- components/store/StoreImageGrid.jsx
+++ components/store/StoreImageGrid.jsx
@@ -1,30 +1,47 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import styled from 'styled-components'
-import { Image } from 'semantic-ui-react'
+import { Button, Image } from 'semantic-ui-react'
 
 const StoreImageGrid = ({ store_uuid }) => {
-  const [storeImageLinkList, setStoreImageLinkList] = useState([])
+  const [storeImageList, setStoreImageList] = useState([])
 
   useEffect(() => {
     if (!store_uuid) return
 
     axios.get(`${process.env.NEXT_PUBLIC_API}/store-image/${store_uuid}`).
-      then(res => setStoreImageLinkList(res.data)).
+      then(res => setStoreImageList(res.data)).
       catch((err) => console.log(err))
   }, [store_uuid])
 
+  function handleImageDelete (image_uuid) {
+    axios.delete(`${process.env.NEXT_PUBLIC_API}/store-image/image/${image_uuid}`, {withCredentials: true})
+      .then(() => window.location.reload())
+      .catch((err) => {
+        alert('이미지 삭제에 실패했습니다.'); console.log(err)
+      })
+  }
+
   return (
-    <StoreImageDiv>
+    <Wrapper>
       {
-        storeImageLinkList.length ? (
-          storeImageLinkList.map(link => {
+        storeImageList.length ? (
+          storeImageList.map(storeImage => {
             return (
-              <div key={link} style={{height: 250, borderRadius: '5px', overflow: 'hidden', margin: '5px'}}>
-                <Image style={{width:"100%", height:"100%", objectFit: "cover"}}
-                       alt={'store_photo'}
-                       src={link ?? 'https://via.placeholder.com/240'}/>
-              </div>
+              <StoreImageDiv key={storeImage.uuid}>
+                <Image
+                  rounded
+                  src={storeImage.link ??
+                    'https://via.placeholder.com/200?text=InPoStack'}
+                  alt={'food_img'}
+                  style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+                />
+                <Button
+                  compact style={{ width: '100%' }}
+                  onClick={() => handleImageDelete(storeImage.uuid)} color={'red'} >
+                  이미지 삭제
+                </Button>
+              </StoreImageDiv>
             )
           })
         ) : (
@@ -33,16 +50,27 @@ const StoreImageGrid = ({ store_uuid }) => {
           </h3>
         )
       }
-    </StoreImageDiv>
+    </Wrapper>
   )
 }
 
 export default StoreImageGrid
 
-const StoreImageDiv = styled.div`
+const Wrapper = styled.div`
   display: grid;
   grid-template-columns: repeat(4, 1fr);
   @media only screen and (max-width: ${({ theme }) => theme.breakpoint.s}) {
     grid-template-columns: repeat(2, 1fr);
   }
+
+  margin-left: -10px;
+  margin-right: -10px;
+
+  margin-bottom: 40px;
+`
+
+const StoreImageDiv = styled.div`
+  padding: 10px;
+  height: 250px;
+  border-radius: 5px;
 `
--- components/store/storeInfoDiv.jsx
+++ components/store/storeInfoDiv.jsx
@@ -1,47 +1,23 @@
-import React, { useEffect, useState } from 'react'
-import styled from 'styled-components'
-import { Divider, Image, List } from 'semantic-ui-react'
+import React from 'react'
+import { Divider, List } from 'semantic-ui-react'
 // import StoreMap from './storeMap';
-import axios from 'axios'
 import { isBlurry } from '../../utils/blurry-check'
 import StoreOpeningHours from './StoreOpeningHours'
+import StoreImageGrid from './StoreImageGrid'
 
 const StoreInfoDiv = ({ storeInfo }) => {
   const uuid = storeInfo.uuid
 
-  const [storeImageLinkList, setStoreImageLinkList] = useState([])
-
-  useEffect(() => {
-    axios.get(`${process.env.NEXT_PUBLIC_API}/store-image/${uuid}`).
-      then(res => setStoreImageLinkList(res.data)).
-      catch((err) => console.log(err))
-  }, [uuid])
-
   return (
-    <div style={{marginBottom: 16}}>
-      <StoreImageGrid>
-        {
-          storeImageLinkList.map(link => {
-            return (
-              <StoreImageDiv key={link}>
-                <Image
-                  rounded
-                  src={link ??
-                    'https://via.placeholder.com/200?text=InPoStack'}
-                  alt={'food_img'}
-                  style={{width:"100%", height:"100%", objectFit: "cover"}}
-                />
-              </StoreImageDiv>
-            )
-          })
-        }
-      </StoreImageGrid>
+    <div style={{ marginBottom: 16 }}>
+
+      <StoreImageGrid store_uuid={uuid}/>
 
       <Divider/>
 
       <List>
         <List.Item>
-          <List.Icon name={'call'} />
+          <List.Icon name={'call'}/>
           <List.Content>
             {
               isBlurry(storeInfo.phone) ? (
@@ -53,30 +29,30 @@ const StoreInfoDiv = ({ storeInfo }) => {
           </List.Content>
         </List.Item>
         <List.Item>
-          <List.Icon name={'home'} />
+          <List.Icon name={'home'}/>
           <List.Content>
-          {
-            isBlurry(storeInfo.address1) ? (
-              <span className={'blurry-text'}>정보 수집중</span>
-            ) : (
-              storeInfo.naver_map_url ? (
-                <a href={storeInfo.naver_map_url} target={'_blank'}
-                   rel={'noreferrer'}>
-                  {storeInfo.address1} {storeInfo.address2}
-                  <span style={{color: 'gray', marginLeft: 8}}>
+            {
+              isBlurry(storeInfo.address1) ? (
+                <span className={'blurry-text'}>정보 수집중</span>
+              ) : (
+                storeInfo.naver_map_url ? (
+                  <a href={storeInfo.naver_map_url} target={'_blank'}
+                     rel={'noreferrer'}>
+                    {storeInfo.address1} {storeInfo.address2}
+                    <span style={{ color: 'gray', marginLeft: 8 }}>
                     (우) {storeInfo.zipcode}
                   </span>
-                </a>
-              ) : (
-                <span>
+                  </a>
+                ) : (
+                  <span>
                   {storeInfo.address1} {storeInfo.address2}
-                  <span style={{color: 'gray', marginLeft: 8}}>
+                    <span style={{ color: 'gray', marginLeft: 8 }}>
                     (우) {storeInfo.zipcode}
                   </span>
                 </span>
+                )
               )
-            )
-          }
+            }
           </List.Content>
           {/* TODO: 클릭하면 펼쳐보이게 */}
           {/*<StoreMap address1={storeInfo.address1} />*/}
@@ -115,20 +91,3 @@ const StoreInfoDiv = ({ storeInfo }) => {
 }
 
 export default StoreInfoDiv
-
-const StoreImageGrid = styled.div`
-  display: grid;
-  grid-template-columns: repeat(4, 1fr);
-  @media only screen and (max-width: ${({ theme }) => theme.breakpoint.s}) {
-    grid-template-columns: repeat(2, 1fr);
-  }
-
-  margin-left: -10px;
-  margin-right: -10px;
-`
-
-const StoreImageDiv = styled.div`
-  padding: 10px;
-  height: 250px;
-  border-radius: 5px;
-`
